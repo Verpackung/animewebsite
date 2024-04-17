482eedb4d6cbdc524ab76eabd0ca80500d34fb52
Verpackung <malsky_wow@web.de>
2024-04-17 12:09:36 +0200

    added onClcik for dropdown items

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,17 +1,27 @@
+import { useState } from "react";
 import Dropdown from "./dropdown/Dropdown";
 import DropdownItem from "./dropdown/DropdownItem";
 import styles from "./filter.module.css";
 
 export default function Filter({ setFilter }) {
-  const items = [1, 2, 3];
+  const items = ["Airing", "Completed", "Not Yet Aired"];
+  const [buttonText, setButtonText] = useState("Airing");
+
+  const handleClick = (e) => {
+    const clickedFilter = e.target.outerText;
+    setButtonText(clickedFilter);
+  };
+
   return (
     <div className={styles.content}>
       <Dropdown
-        buttonText="Dropdown button"
+        buttonText={buttonText}
         content={
           <>
             {items.map((item) => (
-              <DropdownItem key={item}>{`Item ${item}`}</DropdownItem>
+              <DropdownItem key={item} onClick={(e) => handleClick(e)}>
+                {item}
+              </DropdownItem>
             ))}
           </>
         }
