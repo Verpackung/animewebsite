b5318f8990196f6f9214119b46e5ca949b3d0aaa
Verpackung <malsky_wow@web.de>
2024-04-17 15:47:52 +0200

    close dropdown by clicking background

--- src/components/dropdown/Dropdown.jsx
+++ src/components/dropdown/Dropdown.jsx
@@ -1,17 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styles from "./dropdown.module.css";
 import DropdownButton from "./DropdownButton";
 import DropdownContent from "./DropdownContent";
 
 export default function Dropdown({ buttonText, content }) {
   const [open, setOpen] = useState(false);
+  const dropdownRef = useRef();
 
   const toggleDropdown = () => {
     setOpen((open) => !open);
   };
 
+  useEffect(() => {
+    const handler = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setOpen(false);
+      }
+    };
+    document.addEventListener("click", handler);
+
+    return () => {
+      document.removeEventListener("click", handler);
+    };
+  }, []);
+
   return (
-    <div className={styles.dropdown}>
+    <div className={styles.dropdown} ref={dropdownRef}>
       <DropdownButton toggle={toggleDropdown} open={open}>
         {buttonText}
       </DropdownButton>
